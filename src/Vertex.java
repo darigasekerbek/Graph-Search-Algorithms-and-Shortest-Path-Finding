eaf48c2d00126abf05ce643a8cd9a1ad7636ad44
Abylai <221714@astanait.edu.kz>
2023-05-31 21:20:45 +0200

    Comment for vertex

--- src/Vertex.java
+++ src/Vertex.java
@@ -4,18 +4,18 @@ import java.util.List;
 public class Vertex {
     private int id;
     private List<Edge> adjacentEdges;
-    public Vertex(int id) {
+    public Vertex(int id) { // constructor to init a vertex with given id
         this.id = id;
         this.adjacentEdges = new ArrayList<>();
     }
-    public int getId(){
+    public int getId(){ //getter to retrieve id of vertex
         return id;
     }
-    public void addAdjacentEdge(Vertex vertex, int weight){
-        Edge edge = new Edge(vertex, weight);
+    public void addAdjacentEdge(Vertex vertex, int weight){ //method which add an adjacent edge to vertex
+        Edge edge = new Edge(vertex, weight); // it takes vertex and weight as parameters
         adjacentEdges.add(edge);
     }
-    public List<Edge> getAdjacentEdges(){
+    public List<Edge> getAdjacentEdges(){ // method that retrieve list of adj edges of vertex
         return adjacentEdges;
     }
     public String toString(){
