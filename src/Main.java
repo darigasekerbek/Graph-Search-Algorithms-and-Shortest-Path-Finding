dce41206c6964de8391b2939924ed8471f36608f
Abylai <221714@astanait.edu.kz>
2023-05-31 18:30:50 +0200

    Create vertices

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args)
+    {
+        Vertex A = new Vertex(0);
+        Vertex B = new Vertex(1);
+        Vertex C = new Vertex(2);
+        Vertex D = new Vertex(3);
+        Vertex E = new Vertex(4);
+
+        A.addAdjacentEdge(B, 3);
+        A.addAdjacentEdge(C, 3);
+        B.addAdjacentEdge(D, 4);
+        C.addAdjacentEdge(B, 1);
+        C.addAdjacentEdge(E, 5);
+        D.addAdjacentEdge(E, 2);
+
+        WeightedGraph w_graph = new WeightedGraph();
+        w_graph.addVertex(A);
+        w_graph.addVertex(B);
+        w_graph.addVertex(C);
+        w_graph.addVertex(D);
+        w_graph.addVertex(E);
+         }
 }
