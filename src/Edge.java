f8c8bf744f13ca5932920f716a57e5dc12932a31
Abylai <221714@astanait.edu.kz>
2023-05-31 18:06:21 +0200

    Edge

---
+++ src/Edge.java
@@ -0,0 +1,9 @@
+public class Edge {
+    private Vertex destination;
+    private int weight;
+    public Edge(Vertex destination, int weight){
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+}
