342a334a915949ffdb43c93ac210d9ccd27a2dd4
Abylai <221714@astanait.edu.kz>
2023-05-31 21:42:44 +0200

    Comments for BFS

--- src/BFS.java
+++ src/BFS.java
@@ -7,11 +7,11 @@ public class BFS extends Search {
         bfs(source);
     }
     private void bfs(Vertex source){ //breadth first search algorithm
-        Queue<Vertex> q = new LinkedList<>();
+        Queue<Vertex> q = new LinkedList<>(); // q = queue
         visited[source.getId()] = true;
         q.offer(source);
         while (!q.isEmpty()){
-            Vertex curr_V = q.poll();
+            Vertex curr_V = q.poll(); // curr_V = Current_Vertex
             for (Edge edge : curr_V.getAdjacentEdges()){ // traverse adjacent edges of curr vertex
                 Vertex adj_V = edge.getDestination();
                 // if adjacent vertex hasn't been visited, mark it as visited
