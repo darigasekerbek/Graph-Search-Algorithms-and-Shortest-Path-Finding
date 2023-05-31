3a7ef9e68ecae2f8daaa19c3ac81f2a2d6d2f2be
Abylai <221714@astanait.edu.kz>
2023-05-31 18:09:44 +0200

    WeightedGraph

---
+++ src/WeightedGraph.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+public class WeightedGraph {
+    private List<Vertex> vertices;
+    public WeightedGraph(){
+        this.vertices = new ArrayList<>();
+    }
+}
