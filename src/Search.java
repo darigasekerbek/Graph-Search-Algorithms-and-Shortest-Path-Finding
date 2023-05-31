4bd5e7965a7d795e657585e7c63ee6bdba0e55c0
Abylai <221714@astanait.edu.kz>
2023-05-31 21:37:55 +0200

    Comments for Search

--- src/Search.java
+++ src/Search.java
@@ -5,31 +5,32 @@ public class Search {
     protected boolean[] visited;
     protected Vertex source;
     protected WeightedGraph graph;
-    public Search (WeightedGraph graph, Vertex source){
+    public Search (WeightedGraph graph, Vertex source){ //constructor to init search with a weighted graph and source vertex
         this.graph = graph;
         this.source = source;
         visited = new boolean[graph.getVertices().size()];
         edgeTo = new int[graph.getVertices().size()];
         initial();
     }
-    private void initial(){
+    private void initial(){ //helper method which init visited and edgeTo arrays
         for (int i = 0; i< visited.length; i++){
             visited[i] = false;
             edgeTo[i] = -1;
 
         }
     }
-    public List<Vertex> getPath(Vertex destination){
+    public List<Vertex> getPath(Vertex destination){ //method which retrieve path from source vertex to a destination vertex
         List<Vertex> path = new ArrayList<>();
-        if(!visited[destination.getId()]){
+        if(!visited[destination.getId()]){ // if destin vertex isn't visited, return an empty path
             return path;
         }
         int curr = destination.getId();
+        //traverse edgeTo array backwards from destination to source to construct the path
         while (curr != source.getId()){
             path.add(0, graph.getVertices().get(curr));
             curr = edgeTo[curr];
         }
-        path.add(0,source);
+        path.add(0,source); //add source vertex to the path
         return path;
     }
 
